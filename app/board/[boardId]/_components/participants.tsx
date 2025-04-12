e9f6c1e79b0314a75d8842936b849a8317875113
y-0kuda <yutaro.okuda.76@gmail.com>
2025-04-12 13:20:49 +0800

    [Room Participants] 右上のroom内の参加者表示

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,7 +1,54 @@
+"use client";
+
+import { useOthers, useSelf } from "@/liveblocks.config";
+import { connectionIdToColor } from "@/lib/utils";
+
+import { UserAvatar } from "./user-avatar";
+
+// 自分とあと何人右上に表示できるか
+// MAX_SHOWN_USERSが例えば2だと、自分を入れて3つのアバターを表示できる
+const MAX_SHOWN_USERS = 2;
+
 export const Participants = () => {
+  const users = useOthers();
+  const currentUser = useSelf();
+  const hasMoreUsers = users.length > MAX_SHOWN_USERS;
+
   return (
     <div className="absolute top-2 right-2 bg-white rounded-md p-3 h-12 flex items-center shadow-md">
-      Participants
+      {/* MAX人数以内の他の人 -> 自分 -> （余分にいれば）+ 何人 の表示 */}
+      <div className="flex gap-x-2">
+        {/* usersの中にconnectionIdとinfoが入っている */}
+        {/* connectionIdはroomに入るたびに変わる（リフレッシュでも変わる） */}
+        {/* avatar特有のキーを持たせるためにconnectionIdを使用している */}
+        {users.slice(0, MAX_SHOWN_USERS).map(({ connectionId, info }) => {
+          return (
+            <UserAvatar
+              borderColor={connectionIdToColor(connectionId)}
+              key={connectionId}
+              src={info?.picture}
+              name={info?.name}
+              fallback={info?.name?.[0] || "T"}
+            />
+          );
+        })}
+
+        {currentUser && (
+          <UserAvatar
+            borderColor={connectionIdToColor(currentUser.connectionId)}
+            src={currentUser.info?.picture}
+            name={`${currentUser.info?.name} (You)`}
+            fallback={currentUser.info?.name?.[0]}
+          />
+        )}
+
+        {hasMoreUsers && (
+          <UserAvatar
+            name={`${users.length - MAX_SHOWN_USERS} more`}
+            fallback={`+${users.length - MAX_SHOWN_USERS}`}
+          />
+        )}
+      </div>
     </div>
   );
 };
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,15 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+const COLORS = ["#DC2626", "#D97706", "#059669", "#7C3AED", "#DB2777"];
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+// number型を引数で受けて、string型を返す
+export function connectionIdToColor(connectionId: number): string {
+  // connectionIdをCOLORS.lengthで割ったときの余り
+  // 余りは0~4の間となるため、COLORSが割り当てられる
+  return COLORS[connectionId % COLORS.length];
 }
