afef07d3c67a2ac1dcd77be954ccd2538e56ed35
Yutaro Okuda <okuda@Yutaros-MacBook-Air.local>
2025-05-02 12:06:51 +0800

    ロゴとタイトルの変更

--- app/(dashboard)/_components/sidebar/org-sidebar.tsx
+++ app/(dashboard)/_components/sidebar/org-sidebar.tsx
@@ -27,10 +27,10 @@ export const OrgSideBar = () => {
     <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5">
       <Link href="/">
         <div className="flex items-center gap-x-2">
-          <Image src="/logo.svg" alt="logo" height={60} width={60} />
+          <Image src="/logo.svg" alt="logo" height={40} width={40} />
           {/* 特別なフォントを適応するためにあえて初期値と追加の値を設定している */}
           <span className={cn("font-semibold text-2xl", font.className)}>
-            Miro
+            Board
           </span>
         </div>
       </Link>
